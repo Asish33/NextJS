4ee23507b1b3dadd99acf4bccc4e1685195943d1
Balaji_Asish <balajiashish02@gmail.com>
2025-02-07 17:45:32 +0530

    styling

--- src/app/components/LoginButton.tsx
+++ src/app/components/LoginButton.tsx
@@ -3,5 +3,12 @@
 import { signIn } from "next-auth/react";
 
 export default function LoginButton() {
-  return <button onClick={() => signIn(undefined, { callbackUrl: "/" })}>Login</button>;
+  return (
+    <button
+      className="bg-red-500 text-white p-4 m-2 rounded-lg hover:bg-red-400 transition ease-in-out"
+      onClick={() => signIn(undefined, { callbackUrl: "/" })}
+    >
+      Login
+    </button>
+  );
 }
